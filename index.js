ea10db49f55d24791cd4fcdf61150cceb1150446
Chris Stead <cmstead@gmail.com>
2016-11-02 16:59:24 -0700

    Added signature parsing

--- index.js
+++ index.js
@@ -1,15 +1,100 @@
 var signetParser = (function () {
     'use strict';
 
-    function parse (typeStr) {
-        return {
-            'type': typeStr,
-            'subtype': []
+    function terminateSubtype(bracketStack, currentChar) {
+        return (bracketStack.length === 1 && currentChar === ';')
+            || (currentChar === '>' && bracketStack.length === 0)
+    }
+
+    function charIn(chars, char) {
+        var pattern = RegExp('^[' + chars.join('\\') + ']$');
+        return char.match(pattern) !== null;
+    }
+
+    function captureChar(bracketStack, currentChar) {
+        return (bracketStack.length === 0 && currentChar === '>')
+            || (bracketStack.length > 0 && !charIn(['<', ';'], currentChar))
+            || bracketStack.length > 1;
+    }
+
+    function updateStack(bracketStack, currentChar) {
+        if (currentChar === '<') {
+            bracketStack.push(currentChar);
+        } else if (currentChar === '>') {
+            bracketStack.pop();
+        }
+    }
+
+    function buildAppender(bracketStack) {
+        return function (subtypeStr, currentChar) {
+            var capture = captureChar(bracketStack, currentChar);
+            return capture ? subtypeStr + currentChar : subtypeStr;
         };
     }
 
+    function updateSubtypeInfo(bracketStack, subtypeInfo) {
+        return function (subtypeStr, currentChar) {
+            if (terminateSubtype(bracketStack, currentChar)) {
+                subtypeInfo.push(subtypeStr);
+            }
+        }
+    }
+
+    function getUpdatedSubtypeStr(bracketStack, appendOnRule) {
+        return function (subtypeStr, currentChar) {
+            var terminate = terminateSubtype(bracketStack, currentChar);
+            return terminate ? '' : appendOnRule(subtypeStr, currentChar);
+        }
+    }
+
+    function parseSubtype(typeStr) {
+        var subtypeStr = '';
+        var subtypeInfo = [];
+        var bracketStack = [];
+
+        var getSubtypeStr = getUpdatedSubtypeStr(bracketStack, buildAppender(bracketStack));
+        var updateSubtypes = updateSubtypeInfo(bracketStack, subtypeInfo);
+
+        typeStr.split('').forEach(function (currentChar) {
+            updateStack(bracketStack, currentChar);
+            updateSubtypes(subtypeStr, currentChar);
+
+            subtypeStr = getSubtypeStr(subtypeStr, currentChar);
+        });
+
+        return subtypeInfo;
+    }
+
+    function parseType(typeStr) {
+        var typePattern = /^([^\:]+)\:(.+)$/;
+        var typeName = typeStr.replace(typePattern, '$1');
+        var rawType = typeStr.replace(typePattern, '$2');
+
+        return {
+            name: typeName === typeStr ? null : typeName,
+            type: rawType.split('<')[0].replace('[', ''),
+            subtype: parseSubtype(rawType),
+            optional: rawType.match(/^\[[^\]]+\]$/) !== null
+        };
+    }
+
+    function parseParams (token){
+        return token.split(/\s*\,\s*/).map(parseType);
+    }
+
+    function parseSignature(signature) {
+        var parameterTokens = signature.split(/\s*\=\>\s*/);
+
+        if (parameterTokens.length < 2) {
+            throw new Error('Signature must contain an output declaration');
+        }
+
+        return parameterTokens.map(parseParams);
+    }
+
     return {
-        parse: parse
+        parseSignature: parseSignature,
+        parseType: parseType
     };
 })();
 
--- test/parser.test.js
+++ test/parser.test.js
@@ -2,16 +2,65 @@ var assert = require('chai').assert;
 var approvals = require('./utils/approvals.config');
 var parser = require('../index');
 
-function prettyJson (obj) {
+function prettyJson(obj) {
     return JSON.stringify(obj, null, 4);
 }
 
 describe('parser', function () {
 
-    it('should return a simple type object', function () {
-        var result = parser.parse('int');
+    describe('parseType', function () {
+        it('should return a simple type object', function () {
+            var result = parser.parseType('int');
 
-        this.verify(prettyJson(result));
+            this.verify(prettyJson(result));
+        });
+
+        it('should return a type object with subtype info', function () {
+            var result = parser.parseType('array<int>');
+
+            this.verify(prettyJson(result));
+        });
+
+        it('should not mangle nested subtypes', function () {
+            var result = parser.parseType('array<tuple<int>>');
+
+            this.verify(prettyJson(result));
+        });
+
+        it('should split subtype strings on semicolons', function () {
+            var result = parser.parseType('tuple<string;tuple<string;int>>');
+
+            this.verify(prettyJson(result));
+        });
+
+        it('should set optional flag when type is square-bracketed', function () {
+            var result = parser.parseType('[tuple<string;tuple<string;int>>]');
+
+            this.verify(prettyJson(result));
+        });
+
+        it('should set a name when a value is prepended', function () {
+            var result = parser.parseType('test:[tuple<string;tuple<string;int>>]');
+
+            this.verify(prettyJson(result));
+        });
+
+    });
+
+    describe('parseSignature', function () {
+        
+        it('should throw an error if signature contains no arrow', function () {
+            var message = 'Signature must contain an output declaration';
+            assert.throws(parser.parseSignature.bind(null, 'int'), message);
+        });
+
+        it('should parse a correctly formatted signature', function () {
+            var signature = 'name:string, definition:tuple<string;tuple<int;int>> => * => tuple<int;int>';
+            var result = parser.parseSignature(signature);
+
+            this.verify(prettyJson(result));
+        });
+
     });
 
 });
